ddd7faedafff902955aeb2fb1f6909d146d4b3c4
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2025-05-24 23:37:22 +0530

    Add files via upload

---
+++ Leet_1929_Array_Concat.java
@@ -0,0 +1,11 @@
+class Leet_1929_Array_Concat {
+    public int[] getConcatenation(int[] nums) {
+        int N = nums.length;
+        int[] ans = new int[2*N];
+        for(int i=0;i<N;i++){
+            ans[i] = nums[i];
+            ans[i+N] = nums[i];
+        }
+        return ans;
+    }
+}
