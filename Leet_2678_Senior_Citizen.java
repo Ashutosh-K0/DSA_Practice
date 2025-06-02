5f1b6bbb11f780cee9a58fc461f012e3958f41f8
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2025-06-02 22:25:13 +0530

    Add files via upload

---
+++ Leet_2678_Senior_Citizen.java
@@ -0,0 +1,12 @@
+class Leet_2678_Senior_Citizen {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (int i = 0; i < details.length; i++) {
+            int age = Integer.parseInt(details[i].substring(11, 13));
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
