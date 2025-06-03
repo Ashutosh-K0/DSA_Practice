9efca95591de1b24f69f798749d21c6482ff41aa
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2025-06-03 23:56:45 +0530

    Add files via upload

---
+++ Leet_2942_Words_Characters.java
@@ -0,0 +1,13 @@
+class Leet_2942_Words_Characters {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
